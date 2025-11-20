8004fe1268316a600127776f16e8a0d47d105096
Farid | Iamjustzero <imjustzero@duck.com>
2025-11-20 22:12:12 +0700

    hide create report

--- app/Filament/Resources/Reports/Pages/ManageReports.php
+++ app/Filament/Resources/Reports/Pages/ManageReports.php
@@ -13,7 +13,7 @@ class ManageReports extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            // CreateAction::make(),
         ];
     }
 }
