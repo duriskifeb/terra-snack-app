350c6f025b2ba3a1ddf3cd8487ce66d1eabbab9d
Farid | Iamjustzero <imjustzero@duck.com>
2025-11-21 20:21:50 +0700

    change colors theme to red

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,7 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ->brandLogo(asset('images/logoTerraSnack.svg'))
             ->brandLogoHeight('2.5rem')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Red,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
