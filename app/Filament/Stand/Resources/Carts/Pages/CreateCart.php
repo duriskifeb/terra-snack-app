a8fd5aec27ba872ba9b3a4a615132b03d09f1568
Farid | Iamjustzero <imjustzero@duck.com>
2025-11-22 19:04:00 +0700

    add cart resource

---
+++ app/Filament/Stand/Resources/Carts/Pages/CreateCart.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Stand\Resources\Carts\Pages;
+
+use App\Filament\Stand\Resources\Carts\CartResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCart extends CreateRecord
+{
+    protected static string $resource = CartResource::class;
+}
