6cea78260df8a51bf110a15f62ccd4ac193d1e33
Farid | Iamjustzero <imjustzero@duck.com>
2025-11-23 19:48:41 +0700

    hide delete button customization option

--- app/Filament/Resources/CustomizationOptions/CustomizationOptionResource.php
+++ app/Filament/Resources/CustomizationOptions/CustomizationOptionResource.php
@@ -55,7 +55,7 @@ class CustomizationOptionResource extends Resource
             ])
             ->recordActions([
                 EditAction::make(),
-                DeleteAction::make(),
+                // DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
